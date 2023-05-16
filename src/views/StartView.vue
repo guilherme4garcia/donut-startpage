cbd54be79356599334777224c56cb69b277b7630
Guilherme Garcia <guilherme4garcia@gmail.com>
2023-05-16 12:56:47 -0300

    StartView

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -5,15 +5,10 @@
   <main>
     <div>
 
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati placeat id facilis nihil harum totam voluptate assumenda. Aperiam cupiditate voluptatem soluta necessitatibus id aut earum maxime dolor. Nulla, laborum voluptatibus!
-      Nihil quibusdam odit impedit repudiandae ipsa quidem mollitia, similique iure, eos eum commodi expedita non voluptatum suscipit officiis blanditiis neque! Numquam iusto quo harum cupiditate reprehenderit aliquam rem neque doloribus!
-      Adipisci recusandae nobis suscipit aspernatur nisi minus voluptatum accusamus necessitatibus voluptas cupiditate? Laudantium modi voluptas perspiciatis autem corporis dolorem quas facere. Obcaecati repudiandae mollitia sed ab numquam ex ad adipisci.
-      Commodi nulla iste libero veniam, ullam ea magni accusamus officia repellat? Molestiae iure numquam id rem labore laborum, voluptatum ut molestias quas qui voluptas magni rerum error. Natus, tenetur. Voluptatem.
-      Rem ipsa unde facere illo minima eum. Ipsa quisquam ex voluptates eveniet rerum, provident voluptatum sed assumenda temporibus aut ut asperiores exercitationem quibusdam, at tempora unde impedit distinctio laboriosam maxime.</p>
       <div style="display: flex;">
-        <p>guilherme@moog:~$</p>
-        <input type="text" name="" id="">
-
+        <p class="name" style="">guilherme@moog:~$</p>
+        <input style="caret-color:chartreuse;"  type="text" name="" id="" @keypress="">
+        
       </div>
 
     </div>
@@ -21,8 +16,35 @@
 </template>
 
 
-<script></script>
+<script>
+  import { onMounted } from 'vue';
+  export default {
+    components: {
+     
+    },
+    data() {
+      return {
+        
+      };
+    },
+    watch: {
+      banco(newValue) {
+      }
+    },
+    setup() {
+      onMounted(() => {
+
+      })
+    }
+  }
+</script>
 
 <style>
+
+  .name {
+    color:chartreuse; 
+    font-family:Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    margin-right: 0.5rem;
+  }
 
 </style>
